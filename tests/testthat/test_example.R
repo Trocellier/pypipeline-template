4c4e4ffa51df52be209bde311c07f585fb018086
Trocellier <louis.trocellier@gmail.com>
2026-08-29 23:05:44 +0200

    maj ajout de tests

--- tests/testthat/test_example.R
+++
@@ -1,9 +0,0 @@
-# tests/testthat/test_example.R --------------------------------------------- #
-# Author: Louis Trocellier
-# Description: Example test suite verifying basic environment initialization.
-
-# test that the configuration environment is correctly initialized 
-test_that("Environment is correctly initialized", {
-  expect_true(exists("cfg"))
-  expect_true(is.list(cfg))
-})
