0d894c448e9e383add31491b4e6ace5b42e6aeab
Trocellier <louis.trocellier@gmail.com>
2026-08-30 13:35:04 +0200

    maj common fonctions

--- function/common.R
+++ function/common.R
@@ -15,39 +15,7 @@ common_format <- function() {
 }
 
 # Display -------------------------------------------------------------------- #
-# Create a box with default styling
-common_main_box <- function(x, title, status = "primary", solidHeader = TRUE, 
-                       collapsible = TRUE, width = 12) {
-  box(
-    title = title,
-    status = status,
-    solidHeader = solidHeader,
-    collapsible = collapsible,
-    width = width,
-    x
-  )
-}
-
 # TODO: Add other common display functions
 common_display <- function() {
   NULL
-}
-
-# Execution helper ---------------------------------------------------------- #
-# Execute a reactive or function inside tryCatch and return NULL on error.
-# `expr_fn` should be a function (a reactive is a function). If `notify` is
-# TRUE and shiny is available, a notification will be shown on error.
-safe_exec <- function(expr_fn, name = NULL, notify = FALSE) {
-  if (is.null(name)) name <- deparse(substitute(expr_fn))
-  tryCatch(
-    expr_fn(),
-    error = function(e) {
-      msg <- paste0("Error in ", name, ": ", conditionMessage(e))
-      message(msg)
-      if (isTRUE(notify) && requireNamespace("shiny", quietly = TRUE)) {
-        shiny::showNotification(msg, type = "error")
-      }
-      NULL
-    }
-  )
 }
